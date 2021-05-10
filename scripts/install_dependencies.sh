c159eb1d72ade1893706f6626bca094aa9a4249f
Lucas Enos <lucasenos.pro@gmail.com>
2021-05-10 17:03:11 +0200

    pip

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-pip3 install virtualenv
-python -m venv test_venv
+pip install virtualenv
+python3 -m venv test_venv
 . test_venv/bin/activate
-pip3 install --upgrade pip3
-pip3 install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
