d5b2aa07fe2e8de935668e311b44020693362ba1
dogon37 <lucasenos.pro@gmail.com>
2021-05-11 11:57:43 +0200

    stop_flask

--- handlers/routes.py
+++ handlers/routes.py
@@ -6,7 +6,7 @@ def configure_routes(app):
 
     @app.route('/')
     def hello_world():
-        return 'Hello, World! TEST!!!!!!!!!!!!'
+        return 'Hello, World!'
 
     @app.route('/post/test', methods=['POST'])
     def receive_post():
