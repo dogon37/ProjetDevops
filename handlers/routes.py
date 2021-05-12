526b9bd81c4b4579f5dcdefbe4dd4476985eeb5a
dogon37 <lucasenos.pro@gmail.com>
2021-05-12 10:04:30 +0200

    test

--- handlers/routes.py
+++ handlers/routes.py
@@ -6,7 +6,7 @@ def configure_routes(app):
 
     @app.route('/')
     def hello_world():
-        return 'Hello, World! nouvelle version'
+        return 'Hello, World!'
 
     @app.route('/post/test', methods=['POST'])
     def receive_post():
